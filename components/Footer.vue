be500d6aea0f657e38340495d940cbb1cd43fefe
Lucas Chang <lucaskchang@gmail.com>
2024-09-14 17:42:06 -0400

    update automatic route adding function

--- components/Footer.vue
+++ components/Footer.vue
@@ -12,41 +12,28 @@
 </template>
 
 <!--
-add id to routes
 <script setup lang="ts">
-import { useFirestore } from 'vuefire';
-import { collection, getDocs, setDoc } from 'firebase/firestore';
-
-const db = useFirestore();
-const routesCollection = collection(db, 'routes');
-
-const docs = await getDocs(routesCollection);
-docs.forEach(async (doc) => {
-  await setDoc(doc.ref, {
-    ...doc.data(),
-    id: doc.id,
-  });
-  console.log('Document written with ID: ', doc.id);
-});
-</script> -->
-
-<!--
-script to add routes to firestore
-<script setup lang="ts">
-import { collection, addDoc } from 'firebase/firestore';
+import { collection, addDoc, updateDoc, getDocs } from 'firebase/firestore';
 import routes from '~/assets/data/routes.json';
 
 const db = useFirestore();
-
 const routesCollection = collection(db, 'routes');
 
 onMounted(async () => {
-  for (const route of routes) {
-    const docRef = await addDoc(routesCollection, {
-      ...route,
-      ratings: [],
-    });
-    console.log('Document written with ID: ', docRef.id);
+  if ((await getDocs(routesCollection)).empty) {
+    let count = 0;
+    for (const route of routes) {
+      const routeDocRef = await addDoc(routesCollection, {
+        ...route,
+        ratings: [],
+      });
+      await updateDoc(routeDocRef, {
+        id: routeDocRef.id,
+      });
+      count++;
+      console.log(`Document ${count} added with ID: ${routeDocRef.id}`);
+    }
   }
 });
-</script> -->
+</script>
+-->
