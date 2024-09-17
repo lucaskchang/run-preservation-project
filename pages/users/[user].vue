150b0b91fdf59debaf4dbe7f67344c30f8a9c1ee
Lucas Chang <lucaskchang@gmail.com>
2024-09-17 11:27:10 -0400

    edit user page

--- pages/users/[user].vue
+++ pages/users/[user].vue
@@ -22,70 +22,55 @@
         </span>
       </p>
     </div>
-    <div class="mx-auto mt-8 flex flex-col justify-center md:w-2/3 md:flex-row md:space-x-8 2xl:w-1/2">
-      <div>
-        <p class="text-2xl font-bold">
-          All Ratings
-        </p>
-        <div
-          v-for="rating in ratings"
-          :key="rating.id"
-        >
-          <NuxtLink
-            :to="`/routes/${rating.id}`"
+    <div class="mt-4 flex flex-col justify-between space-y-2 md:flex-row md:items-center md:space-y-0">
+      <p class="text-2xl font-bold">
+        Ratings
+      </p>
+      <USelect
+        v-model="sort"
+        leading-icon="i-mdi-sort-variant"
+        :options="sortOptions"
+        class="w-full md:w-1/4 lg:w-1/5"
+      />
+    </div>
+    <div
+      v-for="rating in ratings.slice(page * 25, page * 25 + 25)"
+      :key="rating.id"
+    >
+      <NuxtLink
+        :to="`/routes/${rating.id}`"
+        class="flex flex-row items-center justify-between rounded border-b border-gray-200 p-2 hover:bg-gray-200 md:px-4 dark:border-gray-800 dark:hover:bg-gray-800"
+      >
+        <p class="font-bold md:text-lg">
+          {{ rating.name }}
+          <span
+            class="text-sm"
+            :class="getRatingColor(rating.rating)"
           >
-            <p
-              class="mt-2 font-bold"
-            >
-              {{ rating.name }} <span class="font-normal text-gray-700 dark:text-gray-200"> {{ rating.date }}</span>
-            </p>
-            <p>
-              Rating: <span
-                class="font-bold"
-                :class="getRatingColor(rating.rating)"
-              >{{ rating.rating }}</span>
-            </p>
-          </NuxtLink>
-        </div>
-      </div>
-      <div class="mt-8 md:mt-0">
-        <p class="text-2xl font-bold">
-          Highest Rating
+            {{ rating.rating }}
+          </span>
         </p>
-        <NuxtLink
-          :to="`/routes/${highestRating.id}`"
-        >
-          <p
-            class="mt-2 font-bold"
-          >
-            {{ highestRating.name }} <span class="font-normal text-gray-700 dark:text-gray-200"> {{ highestRating.date }}</span>
-          </p>
-          <p>
-            Rating: <span
-              class="font-bold"
-              :class="getRatingColor(highestRating.rating)"
-            >{{ highestRating.rating }}</span>
-          </p>
-        </NuxtLink>
-        <p class="mt-4 text-2xl font-bold">
-          Lowest Rating
+        <p class="font-semibold text-gray-700 dark:text-gray-200">
+          {{ rating.date }}
         </p>
-        <NuxtLink
-          :to="`/routes/${lowestRating.id}`"
-        >
-          <p
-            class="mt-2 font-bold"
-          >
-            {{ lowestRating.name }} <span class="font-normal text-gray-700 dark:text-gray-200"> {{ lowestRating.date }}</span>
-          </p>
-          <p>
-            Rating: <span
-              class="font-bold"
-              :class="getRatingColor(lowestRating.rating)"
-            >{{ lowestRating.rating }}</span>
-          </p>
-        </NuxtLink>
-      </div>
+      </Nuxtlink>
+    </div>
+    <div class="mt-4 flex flex-row items-center justify-between">
+      <UButton
+        v-if="page > 0"
+        @click="page--"
+      >
+        Previous
+      </UButton>
+      <p>
+        Page {{ page + 1 }}
+      </p>
+      <UButton
+        v-if="page < ratings.length / 25 - 1"
+        @click="page++"
+      >
+        Next
+      </UButton>
     </div>
     <BackButton />
   </div>
@@ -105,6 +90,9 @@ const user = computed(() => {
   const foundUser = users.value.find(user => user.email === email);
   return foundUser ? foundUser : { name: 'Unknown' };
 });
+const page = ref(0);
+const sortOptions = ['Rating (Low-High)', 'Rating (High-Low)', 'Date (Old-New)', 'Date (New-Old)'];
+const sort = ref('Date (New-Old)');
 
 const ratings = computed(() => {
   const ratings = [];
@@ -115,33 +103,27 @@ const ratings = computed(() => {
           ratings.push({
             name: route.name,
             id: route.id,
-            date: new Date(rating.date).toLocaleDateString(),
+            date: new Date(rating.date).toLocaleString(),
             rating: rating.rating,
           });
         }
       }
     }
   }
+  switch (sort.value) {
+    case 'Rating (Low-High)':
+      ratings.sort((a, b) => a.rating - b.rating);
+      break;
+    case 'Rating (High-Low)':
+      ratings.sort((a, b) => b.rating - a.rating);
+      break;
+    case 'Date (Old-New)':
+      ratings.sort((a, b) => new Date(a.date) - new Date(b.date));
+      break;
+    case 'Date (New-Old)':
+      ratings.sort((a, b) => new Date(b.date) - new Date(a.date));
+      break;
+  }
   return ratings;
-});
-
-const highestRating = computed(() => {
-  let highest = { rating: -1 };
-  for (const rating of ratings.value) {
-    if (rating.rating > highest.rating) {
-      highest = rating;
-    }
-  }
-  return highest;
-});
-
-const lowestRating = computed(() => {
-  let lowest = { rating: 101 };
-  for (const rating of ratings.value) {
-    if (rating.rating < lowest.rating) {
-      lowest = rating;
-    }
-  }
-  return lowest;
 });
 </script>
